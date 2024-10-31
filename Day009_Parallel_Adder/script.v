de95fef40233798ef3d68d653bb0a9ae7d50927a
Suman <shumansswain@gmail.com>
2024-10-31 09:44:39 +0000

    Day 9

---
+++ Day009_Parallel_Adder/script.v
@@ -0,0 +1,39 @@
+module full_adder (
+    input wire a,
+    input wire b,
+    input wire Cin,
+    output wire sum,
+    output wire Cout
+);
+
+    assign sum = a ^ b ^ Cin;
+    assign Cout = (a & b) + (Cin & (a ^ b));
+    
+endmodule
+
+module parralel_adder#(parameter WIDTH = 4)(
+   input wire [WIDTH - 1 : 0] a,
+   input wire [WIDTH - 1 : 0] b,
+   input wire Cin,
+   output wire [WIDTH - 1 : 0] sum,
+   output wire Cout
+);
+
+    wire [WIDTH : 0] carry;
+    assign carry[0] = Cin;
+    assign Cout = carry[WIDTH];
+    genvar i;
+
+    generate 
+        for(i = 0;i < WIDTH; i = i + 1) begin : fa_inst
+            full_adder inst(
+                .a(a[i]),
+                .b(b[i]),
+                .Cin(carry[i]),
+                .sum(sum[i]),
+                .Cout(carry[i + 1])
+            );
+        end
+    endgenerate
+    
+endmodule
---
+++ Day009_Parallel_Adder/test_bench.v
@@ -0,0 +1,41 @@
+module Day009_tb ();
+
+    reg clock;
+
+    //input
+    reg [3 : 0] a,b; 
+    reg cin;
+
+    //output
+    wire [3 : 0] sum; 
+    wire cout;
+
+
+    initial begin
+        clock = 0;
+        forever #2 clock = ~clock;
+    end
+
+    initial begin
+            a <= 3'b010; b <= 3'b101; cin <= 1;
+        #4  a <= 3'b010; b <= 3'b101; cin <= 0;
+        #4  a <= 3'b001; b <= 3'b010; cin <= 1;
+        #4  a <= 3'b001; b <= 3'b010; cin <= 0;
+        #4  a <= 3'b001; b <= 3'b001; cin <= 1;
+        #4  a <= 3'b001; b <= 3'b001; cin <= 0;
+        #4
+        $finish;
+
+    end
+
+    always @(posedge clock) $display("a = %d b = %d Cin = %d sum = %d Cout = %d ", a, b, Cin, sum, Cout);
+
+    parralel_adder pa_inst(
+        .a(a),
+        .b(b),
+        .Cin(Cin),
+        .sum(sum),
+        .Cout(Cout)
+    );
+
+endmodule
